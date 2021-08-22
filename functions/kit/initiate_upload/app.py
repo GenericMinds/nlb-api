651ccbb25e723b6bcc3f2e9b63a7137a2735eda8
Torrance Graham <torrance.m.graham@gmail.com>
2021-08-22 13:00:32 -0400

    renamed asset model to kit

--- functions/kit/initiate_upload/app.py
+++ functions/kit/initiate_upload/app.py
@@ -8,25 +8,25 @@ import humps
 from botocore.client import BaseClient
 from botocore.exceptions import ClientError
 from layers.shared.common_utils.response_utility import ResponseUtility
-from layers.shared.common_models.asset_models import AssetMetaData, AssetPresignedResponse
+from layers.shared.common_models.kit_models import Kit, KitPresignedResponse
 
 
 def handler(event, context):
     body = ujson.loads(event['body'])
     body = humps.decamelize(body)
-    asset_data = AssetMetaData(**body, id=uuid.uuid4().hex)
-    response = get_presigned_url(asset_data)
+    kit = Kit(**body, file_name=body['title'].replace(" ", ""))
+    response = get_presigned_url(kit)
     return ResponseUtility.create_response(response, 201)
 
 
-def get_presigned_url(file_data: AssetMetaData) -> AssetPresignedResponse:
+def get_presigned_url(kit: Kit) -> KitPresignedResponse:
     request = {
         'Bucket': os.environ['ASSET_BUCKET'],
-        'Key': f"{file_data.asset_type}/{file_data.id}/{file_data.file_name}",
-        'ContentType': file_data.content_type,
+        'Key': f"kits/{kit.kit_type}/{kit.file_name}/{kit.file_name}.zip",
+        'ContentType': "application/zip",
         'Metadata': {
-            'title': file_data.title,
-            'description': file_data.description,
+            'title': kit.title,
+            'description': kit.description,
         }
     }
 
@@ -36,4 +36,4 @@ def get_presigned_url(file_data: AssetMetaData) -> AssetPresignedResponse:
     except ClientError as e:
         logging.error(e)
         raise e
-    return AssetPresignedResponse(asset_id=file_data.id, presigned_url=presigned_url)
+    return KitPresignedResponse(file_name=kit.file_name, presigned_url=presigned_url)
